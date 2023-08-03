7895b2e7d879fe74d440f1e5f0c072dc5b98bef4
Kyle Laverty <lavekyl@gmail.com>
2023-08-03 16:39:19 -0400

    Removed comments, added attributes and keywords

--- build/banner/index.asset.php
+++ build/banner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b57ffa3bb9b1b7ba742e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '74d00943bc266a9ae72f');
